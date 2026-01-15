250ddaab2e7d76e68ad910e90c68d6ebfab8cce1
glutzer <glutzer@glutzer.com>
2026-01-15 12:46:33 -0600

    make classes nullable

--- Systems/WorldMap/EntityLayer/EntityMapComponent.cs
+++ Systems/WorldMap/EntityLayer/EntityMapComponent.cs
@@ -1,110 +1,107 @@
-﻿using System;
+using System;
 using System.Text;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
 
-#nullable disable
+namespace Vintagestory.GameContent;
 
-namespace Vintagestory.GameContent
+#nullable enable
+
+public class EntityMapComponent : MapComponent
 {
-    public class EntityMapComponent : MapComponent
+    public Entity entity;
+    private readonly MeshRef quadModel;
+    public LoadedTexture texture;
+
+    private Vec2f viewPos = new();
+    private readonly Matrixf mvMat = new();
+
+    private readonly int color;
+
+    public EntityMapComponent(ICoreClientAPI capi, LoadedTexture texture, Entity entity, string? color = null) : base(capi)
     {
-        public Entity entity;
-        internal MeshRef quadModel;
-        public LoadedTexture Texture;
+        quadModel = capi.Render.UploadMesh(QuadMeshUtil.GetQuad());
+        this.texture = texture;
+        this.entity = entity;
+        this.color = color == null ? 0 : (ColorUtil.Hex2Int(color) | (255 << 24));
+    }
 
-        Vec2f viewPos = new Vec2f();
-        Matrixf mvMat = new Matrixf();
+    public override void Render(GuiElementMap map, float dt)
+    {
+        IPlayer? player = (entity as EntityPlayer)?.Player;
+        if (player?.WorldData?.CurrentGameMode == EnumGameMode.Spectator == true && capi.World.Player != player) return;
+        if (entity is EntityPlayer { Controls.Sneak: true } && player != capi.World.Player) return;
 
-        int color;
+        if (texture.Disposed || quadModel.Disposed) return;
 
-        public EntityMapComponent(ICoreClientAPI capi, LoadedTexture texture, Entity entity, string color = null) : base(capi)
+        map.TranslateWorldPosToViewPos(entity.Pos.XYZ, ref viewPos);
+
+        float x = (float)(map.Bounds.renderX + viewPos.X);
+        float y = (float)(map.Bounds.renderY + viewPos.Y);
+
+        ICoreClientAPI api = map.Api;
+
+        capi.Render.GlToggleBlend(true);
+
+        IShaderProgram prog = api.Render.GetEngineShader(EnumShaderProgram.Gui);
+        if (color == 0)
         {
-            quadModel = capi.Render.UploadMesh(QuadMeshUtil.GetQuad());
-            this.Texture = texture;
-            this.entity = entity;
-            this.color = color == null ? 0 : (ColorUtil.Hex2Int(color) | 255 << 24);
+            prog.Uniform("rgbaIn", ColorUtil.WhiteArgbVec);
+        }
+        else
+        {
+            Vec4f vec = new();
+            ColorUtil.ToRGBAVec4f(color, ref vec);
+            prog.Uniform("rgbaIn", vec);
         }
 
-        public override void Render(GuiElementMap map, float dt)
+        prog.Uniform("applyColor", 0);
+        prog.Uniform("extraGlow", 0);
+        prog.Uniform("noTexture", 0f);
+        prog.BindTexture2D("tex2d", texture.TextureId, 0);
+
+        mvMat
+            .Set(api.Render.CurrentModelviewMatrix)
+            .Translate(x, y, 60f)
+            .Scale(texture.Width, texture.Height, 0f)
+            .Scale(0.5f, 0.5f, 0f)
+            .RotateZ(-entity.Pos.Yaw + (180f * GameMath.DEG2RAD))
+        ;
+
+        prog.UniformMatrix("projectionMatrix", api.Render.CurrentProjectionMatrix);
+        prog.UniformMatrix("modelViewMatrix", mvMat.Values);
+
+        api.Render.RenderMesh(quadModel);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        quadModel.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public override void OnMouseMove(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
+    {
+        Vec2f viewPos = new();
+        mapElem.TranslateWorldPosToViewPos(entity.Pos.XYZ, ref viewPos);
+
+        double mouseX = args.X - mapElem.Bounds.renderX;
+        double mouseY = args.Y - mapElem.Bounds.renderY;
+        double sc = GuiElement.scaled(5);
+
+        if (Math.Abs(viewPos.X - mouseX) < sc && Math.Abs(viewPos.Y - mouseY) < sc)
         {
-            var player = (entity as EntityPlayer)?.Player;
-            if (player?.WorldData?.CurrentGameMode == EnumGameMode.Spectator == true && capi.World.Player != player) return;
-            if ((entity as EntityPlayer)?.Controls.Sneak == true && player != capi.World.Player) return;
-
-            map.TranslateWorldPosToViewPos(entity.Pos.XYZ, ref viewPos);
-
-            float x = (float)(map.Bounds.renderX + viewPos.X);
-            float y = (float)(map.Bounds.renderY + viewPos.Y);
-            
-            ICoreClientAPI api = map.Api;
-
-            if (Texture.Disposed) throw new Exception("Fatal. Trying to render a disposed texture");
-            if (quadModel.Disposed) throw new Exception("Fatal. Trying to render a disposed texture");
-
-            capi.Render.GlToggleBlend(true);
-
-            IShaderProgram prog = api.Render.GetEngineShader(EnumShaderProgram.Gui);
-            if (color == 0)
+            if (entity is EntityPlayer eplr)
             {
-                prog.Uniform("rgbaIn", ColorUtil.WhiteArgbVec);
-            } else
-            {
-                Vec4f vec = new Vec4f();
-                ColorUtil.ToRGBAVec4f(color, ref vec);
-                prog.Uniform("rgbaIn", vec);
+                hoverText.AppendLine("Player " + capi.World.PlayerByUid(eplr.PlayerUID)?.PlayerName);
             }
-
-            prog.Uniform("applyColor", 0);
-            prog.Uniform("extraGlow", 0);
-            prog.Uniform("noTexture", 0f);
-            prog.BindTexture2D("tex2d", Texture.TextureId, 0);
-
-            mvMat
-                .Set(api.Render.CurrentModelviewMatrix)
-                .Translate(x, y, 60)
-                .Scale(Texture.Width, Texture.Height, 0)
-                .Scale(0.5f, 0.5f, 0)
-                .RotateZ(-entity.Pos.Yaw + 180 * GameMath.DEG2RAD)
-            ;
-
-            prog.UniformMatrix("projectionMatrix", api.Render.CurrentProjectionMatrix);
-            prog.UniformMatrix("modelViewMatrix", mvMat.Values);
-
-            api.Render.RenderMesh(quadModel);
-        }
-
-        public override void Dispose()
-        {
-            base.Dispose();
-
-            quadModel.Dispose();
-        }
-
-
-        public override void OnMouseMove(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
-        {
-            Vec2f viewPos = new Vec2f();
-            mapElem.TranslateWorldPosToViewPos(entity.Pos.XYZ, ref viewPos);
-
-            double mouseX = args.X - mapElem.Bounds.renderX;
-            double mouseY = args.Y - mapElem.Bounds.renderY;
-            double sc = GuiElement.scaled(5);
-
-            if (Math.Abs(viewPos.X - mouseX) < sc && Math.Abs(viewPos.Y - mouseY) < sc)
+            else
             {
-                EntityPlayer eplr = entity as EntityPlayer;
-                if (eplr != null)
-                {
-                    hoverText.AppendLine("Player " + capi.World.PlayerByUid(eplr.PlayerUID)?.PlayerName);
-                } else
-                {
-                    hoverText.AppendLine(entity.GetName());
-                }
+                hoverText.AppendLine(entity.GetName());
             }
         }
     }
-
 }
--- Systems/WorldMap/EntityLayer/PlayerMapLayer.cs
+++ Systems/WorldMap/EntityLayer/PlayerMapLayer.cs
@@ -1,173 +1,155 @@
-﻿using Cairo;
+using Cairo;
 using System.Collections.Generic;
 using System.Text;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 
-#nullable disable
+#nullable enable
 
-namespace Vintagestory.GameContent
+namespace Vintagestory.GameContent;
+
+public class PlayerMapLayer : MarkerMapLayer
 {
+    private readonly Dictionary<IPlayer, EntityMapComponent> mapComps = new();
+    private readonly ICoreClientAPI? capi;
 
-    public class PlayerMapLayer : MarkerMapLayer
+    private LoadedTexture? ownTexture;
+    private LoadedTexture? otherTexture;
+
+    public override string Title => "Players";
+    public override EnumMapAppSide DataSide => EnumMapAppSide.Client;
+    public override string LayerGroupCode => "terrain";
+
+    public PlayerMapLayer(ICoreAPI api, IWorldMapManager mapsink) : base(api, mapsink)
     {
-        Dictionary<IPlayer, EntityMapComponent> MapComps = new Dictionary<IPlayer, EntityMapComponent>();
-        ICoreClientAPI capi;
-        LoadedTexture ownTexture;
-        LoadedTexture otherTexture;
+        capi = api as ICoreClientAPI;
+    }
 
-        public override string Title => "Players";
-        public override EnumMapAppSide DataSide => EnumMapAppSide.Client;
-
-        public override string LayerGroupCode => "terrain";
-
-        public PlayerMapLayer(ICoreAPI api, IWorldMapManager mapsink) : base(api, mapsink)
+    private void Event_PlayerDespawn(IClientPlayer byPlayer)
+    {
+        if (mapComps.TryGetValue(byPlayer, out EntityMapComponent? mp))
         {
-            capi = (api as ICoreClientAPI);
+            mp.Dispose();
+            mapComps.Remove(byPlayer);
+        }
+    }
+
+    private void Event_PlayerSpawn(IClientPlayer byPlayer)
+    {
+        if (capi.World.Config.GetBool("mapHideOtherPlayers", false) && byPlayer.PlayerUID != capi.World.Player.PlayerUID)
+        {
+            return;
         }
 
-        private void Event_PlayerDespawn(IClientPlayer byPlayer)
+        if (mapSink.IsOpened && !mapComps.ContainsKey(byPlayer))
         {
-            if (MapComps.TryGetValue(byPlayer, out EntityMapComponent mp))
-            {
-                mp.Dispose();
-                MapComps.Remove(byPlayer);
-            }
+            EntityMapComponent cmp = new(capi, otherTexture, byPlayer.Entity);
+            mapComps[byPlayer] = cmp;
+        }
+    }
+
+    public override void OnLoaded()
+    {
+        if (capi != null)
+        {
+            // Only client side.
+            capi.Event.PlayerEntitySpawn += Event_PlayerSpawn;
+            capi.Event.PlayerEntityDespawn += Event_PlayerDespawn;
+        }
+    }
+
+    public override void OnMapOpenedClient()
+    {
+        int size = (int)GuiElement.scaled(32);
+
+        if (ownTexture == null)
+        {
+            ImageSurface surface = new(Format.Argb32, size, size);
+            Context ctx = new(surface);
+            ctx.SetSourceRGBA(0, 0, 0, 0);
+            ctx.Paint();
+            capi.Gui.Icons.DrawMapPlayer(ctx, 0, 0, size, size, new double[] { 0, 0, 0, 1 }, new double[] { 1, 1, 1, 1 });
+
+            ownTexture = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(surface, false), size / 2, size / 2);
+            ctx.Dispose();
+            surface.Dispose();
         }
 
-        private void Event_PlayerSpawn(IClientPlayer byPlayer)
+        if (otherTexture == null)
         {
-            if (capi.World.Config.GetBool("mapHideOtherPlayers", false) && byPlayer.PlayerUID != capi.World.Player.PlayerUID)
-            {
-                return;
-            }
-
-            if (mapSink.IsOpened && !MapComps.ContainsKey(byPlayer))
-            {
-                EntityMapComponent cmp = new EntityMapComponent(capi, otherTexture, byPlayer.Entity);
-                MapComps[byPlayer] = cmp;
-            }
+            ImageSurface surface = new(Format.Argb32, size, size);
+            Context ctx = new(surface);
+            ctx.SetSourceRGBA(0, 0, 0, 0);
+            ctx.Paint();
+            capi.Gui.Icons.DrawMapPlayer(ctx, 0, 0, size, size, new double[] { 0.3, 0.3, 0.3, 1 }, new double[] { 0.7, 0.7, 0.7, 1 });
+            otherTexture = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(surface, false), size / 2, size / 2);
+            ctx.Dispose();
+            surface.Dispose();
         }
 
-        public override void OnLoaded()
+        foreach (IPlayer player in capi.World.AllOnlinePlayers)
         {
-            if (capi != null)
+            if (mapComps.TryGetValue(player, out EntityMapComponent? cmp))
             {
-                // Only client side
-                capi.Event.PlayerEntitySpawn += Event_PlayerSpawn;
-                capi.Event.PlayerEntityDespawn += Event_PlayerDespawn;
+                cmp?.Dispose();
+                mapComps.Remove(player);
             }
+
+            if (player.Entity == null)
+            {
+                capi.World.Logger.Warning("Can't add player {0} to world map, missing entity :<", player.PlayerUID);
+                continue;
+            }
+
+            if (capi.World.Config.GetBool("mapHideOtherPlayers", false) && player.PlayerUID != capi.World.Player.PlayerUID) continue;
+
+            cmp = new EntityMapComponent(capi, player == capi.World.Player ? ownTexture : otherTexture, player.Entity);
+
+            mapComps[player] = cmp;
+        }
+    }
+
+    public override void Render(GuiElementMap mapElem, float dt)
+    {
+        if (!Active) return;
+
+        foreach (KeyValuePair<IPlayer, EntityMapComponent> val in mapComps)
+        {
+            val.Value.Render(mapElem, dt);
+        }
+    }
+
+    public override void OnMouseMoveClient(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
+    {
+        if (!Active) return;
+
+        foreach (KeyValuePair<IPlayer, EntityMapComponent> val in mapComps)
+        {
+            val.Value.OnMouseMove(args, mapElem, hoverText);
+        }
+    }
+
+    public override void OnMouseUpClient(MouseEvent args, GuiElementMap mapElem)
+    {
+        if (!Active) return;
+
+        foreach (KeyValuePair<IPlayer, EntityMapComponent> val in mapComps)
+        {
+            val.Value.OnMouseUpOnElement(args, mapElem);
+        }
+    }
+
+    public override void Dispose()
+    {
+        foreach (KeyValuePair<IPlayer, EntityMapComponent> val in mapComps)
+        {
+            val.Value?.Dispose();
         }
 
+        ownTexture?.Dispose();
+        ownTexture = null;
 
-        public override void OnMapOpenedClient()
-        {
-            int size = (int)GuiElement.scaled(32);
-
-            if (ownTexture == null)
-            {
-                ImageSurface surface = new ImageSurface(Format.Argb32, size, size);
-                Context ctx = new Context(surface);
-                ctx.SetSourceRGBA(0, 0, 0, 0);
-                ctx.Paint();
-                capi.Gui.Icons.DrawMapPlayer(ctx, 0, 0, size, size, new double[] { 0, 0, 0, 1 }, new double[] { 1, 1, 1, 1 });
-                
-                ownTexture = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(surface, false), size/2, size / 2);
-                ctx.Dispose();
-                surface.Dispose();
-            }
-            
-            if (otherTexture == null)
-            {
-                ImageSurface surface = new ImageSurface(Format.Argb32, size, size);
-                Context ctx = new Context(surface);
-                ctx.SetSourceRGBA(0, 0, 0, 0);
-                ctx.Paint();
-                capi.Gui.Icons.DrawMapPlayer(ctx, 0, 0, size, size, new double[] { 0.3, 0.3, 0.3, 1 }, new double[] { 0.7, 0.7, 0.7, 1 });
-                otherTexture = new LoadedTexture(capi, capi.Gui.LoadCairoTexture(surface, false), size / 2, size / 2);
-                ctx.Dispose();
-                surface.Dispose();
-            }
-
-
-
-            foreach (IPlayer player in capi.World.AllOnlinePlayers)
-            {
-
-                if (MapComps.TryGetValue(player, out EntityMapComponent cmp))
-                {
-                    cmp?.Dispose();
-                    MapComps.Remove(player);
-                }
-
-
-                if (player.Entity == null)
-                {
-                    capi.World.Logger.Warning("Can't add player {0} to world map, missing entity :<", player.PlayerUID);
-                    continue;
-                }
-
-                if (capi.World.Config.GetBool("mapHideOtherPlayers", false) && player.PlayerUID != capi.World.Player.PlayerUID) continue;
-
-
-                cmp = new EntityMapComponent(capi, player == capi.World.Player ? ownTexture : otherTexture, player.Entity);
-
-                MapComps[player] = cmp;
-            }
-        }
-
-
-        public override void Render(GuiElementMap mapElem, float dt)
-        {
-            if (!Active) return;
-
-            foreach (var val in MapComps)
-            {
-                val.Value.Render(mapElem, dt);
-            }
-        }
-
-        public override void OnMouseMoveClient(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
-        {
-            if (!Active) return;
-
-            foreach (var val in MapComps)
-            {
-                val.Value.OnMouseMove(args, mapElem, hoverText);
-            }
-        }
-
-        public override void OnMouseUpClient(MouseEvent args, GuiElementMap mapElem)
-        {
-            if (!Active) return;
-
-            foreach (var val in MapComps)
-            {
-                val.Value.OnMouseUpOnElement(args, mapElem);
-            }
-        }
-
-        public override void OnMapClosedClient()
-        {
-            //Dispose();
-            //MapComps.Clear();
-        }
-
-
-        public override void Dispose()
-        {
-            foreach (var val in MapComps)
-            {
-                val.Value?.Dispose();
-            }
-
-            ownTexture?.Dispose();
-            ownTexture = null;
-            otherTexture?.Dispose();
-            otherTexture = null;
-        }
-
-
-
+        otherTexture?.Dispose();
+        otherTexture = null;
     }
 }
